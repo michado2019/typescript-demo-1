305b2196c92efe8b3a6ea363f38917df54823ee6
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-06-06 12:48:20 +0100

    typing props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Greet from './components/Greet';
 
 function App() {
   return (
     <div className="App">
-      App
+      <Greet name="Mike" />
     </div>
   );
 }
